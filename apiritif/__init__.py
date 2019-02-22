c474d2314c6aefb50dc98758e1d1c6d395dd19bd
Dimitri Pribysh <dmand@yandex.ru>
2019-02-22 13:05:39 +0300

    Bring back license notice, reorganize

--- apiritif/__init__.py
+++ apiritif/__init__.py
@@ -1,7 +1,21 @@
-from apiritif.utilities import *
-from apiritif.utils import headers_as_text, assert_regexp, assert_not_regexp, log
+
+"""
+This is a toplevel package of Apiritif tool
+Copyright 2017 BlazeMeter Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from .csv import CSVReaderPerThread
 from .thread import put_into_thread_store, get_from_thread_store
 from .http import http, transaction, transaction_logged, recorder, TransactionStarted, TransactionEnded
 from .http import Request, Assertion, AssertionFailure
+from .utilities import *
+from .utils import headers_as_text, assert_regexp, assert_not_regexp, log
