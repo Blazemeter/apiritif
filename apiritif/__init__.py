17343aa25e3dc2929818541f97b63a4b7a371abc
greyfenrir <grey.fenrir@gmail.com>
2019-02-22 13:02:50 +0300

    return imports back

--- apiritif/__init__.py
+++ apiritif/__init__.py
@@ -1,3 +1,6 @@
+from apiritif.utilities import *
+from apiritif.utils import headers_as_text, assert_regexp, assert_not_regexp, log
+
 from .csv import CSVReaderPerThread
 from .thread import put_into_thread_store, get_from_thread_store
 from .http import http, transaction, transaction_logged, recorder, TransactionStarted, TransactionEnded
