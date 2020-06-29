c1ae5783e893fca786caf43820ec6c8ffb3b66bd
corvustristis <clnlbsmrn@gmail.com>
2020-06-29 10:22:29 +0300

    Fix

--- apiritif/csv.py
+++ apiritif/csv.py
@@ -19,7 +19,7 @@ import threading
 
 import unicodecsv as csv
 from itertools import cycle, islice
-from chardet import detect
+from chardet.universaldetector import UniversalDetector
 
 import apiritif.thread as thread
 from apiritif.utils import NormalShutdown
@@ -86,7 +86,8 @@ class CSVReaderPerThread(Reader):  # processes multi-thread specific
 
 
 class CSVReader(Reader):
-    def __init__(self, filename, step=1, first=0, fieldnames=None, delimiter=None, loop=True, quoted=False, encoding=None):
+    def __init__(self, filename, step=1, first=0, fieldnames=None, delimiter=None, loop=True, quoted=False,
+                 encoding=None):
         self.step = step
         self.first = first
         self.csv = {}
@@ -99,7 +100,13 @@ class CSVReader(Reader):
         format_params["quoting"] = csv.QUOTE_MINIMAL if quoted else csv.QUOTE_NONE
 
         if not encoding:
-            encoding = detect(self.fds.read())['encoding']
+            detector = UniversalDetector()
+            for line in self.fds.readlines():
+                detector.feed(line)
+                if detector.done:
+                    break
+            detector.close()
+            encoding = detector.result['encoding']
             self.fds.seek(0)
 
         self._reader = csv.DictReader(self.fds, encoding=encoding, fieldnames=fieldnames, **format_params)
