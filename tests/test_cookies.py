c62598a76734d321c46c918b389cf27f0c3d51d4
Alla Levental <alla.levental@broadcom.com>
2020-12-07 14:17:40 +0300

    Fix test * add sample tests * add sample certificate file

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,9 +1,23 @@
 from unittest import TestCase
+import os
 
 from apiritif import http
 
 
-class TestCookies(TestCase):
+class TestResponce(TestCase):
     def test_cookies(self):
         response = http.get('http://httpbin.org/cookies/set?name=value', cookies={"foo": "bar"})
+        response.assert_ok()
+
+    def test_cert_client(self):
+        # client certificate configuration. requires cert file and password. public server: https://badssl.com/
+        cert = (os.path.join(os.path.dirname(__file__), "resources/data/badssl.com-client.p12"), None)
+        response = http.get('http://badssl.test', cert=cert)
+        response.assert_ok()
+
+    def test_cert_server(self):
+        # server certificate configuration. requires cert and key files. must have a local server to run
+        cert = (os.path.join(os.path.dirname(__file__), "resources/data/server_cert.pem"),
+                os.path.join(os.path.dirname(__file__), "resources/data/server_key.pem"))
+        response = http.get('http://badssl.test', cert=cert)
         response.assert_ok()
