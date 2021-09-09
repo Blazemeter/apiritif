187b33fee07a2a640cee14b7ef293d8ea4822198
Andrey Pokhilko <apc4@ya.ru>
2021-09-09 16:39:30 +0300

    Configure version requirement

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     docs_url='https://github.com/Blazemeter/apiritif',
 
     install_requires=[
-        'nose', 'pytest', 'requests>=2.24.0', 'jsonpath-ng', 'lxml',
+        'nose', 'pytest>=6.2.5', 'requests>=2.24.0', 'jsonpath-ng', 'lxml',
         'unicodecsv', 'cssselect', 'chardet', 'pyopenssl'
     ],
 
