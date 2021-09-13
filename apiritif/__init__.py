4e1ba9af3777fb82b62cc6d3351a878f88bdd15e
Alla Levental <alla.levental@broadcom.com>
2021-09-13 14:44:58 +0300

    Fix

--- apiritif/__init__.py
+++ apiritif/__init__.py
@@ -21,5 +21,3 @@ from .utilities import *
 from .utils import headers_as_text, assert_regexp, assert_not_regexp, log
 # DEPRECATED
 from .thread import get_logging_handlers, set_logging_handlers, external_log
-
-__version__ = "0.9.8"
