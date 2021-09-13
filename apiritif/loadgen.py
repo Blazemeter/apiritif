66dd2f15de0ca662525eb9fb07802491e49597a2
Alla Levental <alla.levental@broadcom.com>
2021-09-13 13:05:52 +0300

    Add version check

--- apiritif/loadgen.py
+++ apiritif/loadgen.py
@@ -519,8 +519,13 @@ def cmdline_to_params():
     parser.add_option('', '--hold-for', action='store', type="float", default=0)
     parser.add_option('', '--result-file-template', action='store', type="str", default="result-%s.csv")
     parser.add_option('', '--verbose', action='store_true', default=False)
+    parser.add_option('', "--version", action='store_true', default=False)
     opts, args = parser.parse_args()
     log.debug("%s %s", opts, args)
+
+    if opts.version:
+        print(apiritif.VERSION)
+        sys.exit(0)
 
     params = Params()
     params.concurrency = opts.concurrency
